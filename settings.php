325523466a2109b8975ebb07cf901dc969931f66
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-12-19 11:29:00 +0000

    Removed admin_setting_pickroles definition as this is in Moodle 2

--- settings.php
+++ settings.php
@@ -1,7 +1,5 @@
 <?php
 
-//this will be standard in moodle 2
-if (!class_exists('admin_setting_pickroles')) { require_once('adminsettingpickroles.php'); }
 
 
 $settings->add(new admin_setting_pickroles('block_messageteacher_roles', get_string('teachersinclude', 'block_messageteacher'),
