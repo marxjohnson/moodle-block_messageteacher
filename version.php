41c5ce508730f822ff3f5ac5fb9c8303a3acd597
Mark Johnson <mark@barrenfrozenwasteland.com>
2013-07-07 17:19:40 +0100

    Bumped required version to 2.5 due to new-style javascript

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013070600;
-$plugin->requires = 2011120500;
+$plugin->requires = 2013051400;
 $plugin->component = 'block_messageteacher';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.2.2';
